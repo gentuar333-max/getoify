3d942931be3435e8c7a968e4931ac74103ac3cee
Gentuar <gentuar333@gmail.com>
2026-05-26 23:11:17 +0200

    update index

--- test.js
+++ test.js
@@ -1,15 +1,9 @@
 const axios = require('axios');
 
-axios.post('https://getoify-test.myshopify.com/admin/api/2026-01/webhooks.json', {
-  webhook: {
-    topic: 'products/create',
-    address: 'https://getoify.vercel.app/webhook/product-create',
-    format: 'json'
-  }
-}, {
-  headers: {
-    'X-Shopify-Access-Token': 'shpua_ef789f03a9d1b35c5a8310547fa77a55',
-    'Content-Type': 'application/json'
+axios.get('https://getoify.vercel.app/locales', {
+  params: {
+    shop: 'getoify-test.myshopify.com',
+    token: 'shpua_ef789f03a9d1b35c5a8310547fa77a55'
   }
 })
 .then(r => console.log(JSON.stringify(r.data, null, 2)))
