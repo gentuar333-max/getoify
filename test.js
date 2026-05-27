6994bf3d6b6def81b36a1c9ad71c8b3d8217bd80
Gentuar <gentuar333@gmail.com>
2026-05-27 12:32:55 +0200

    add frontend

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
 const axios = require('axios');
 
-axios.post('https://getoify.vercel.app/bulk-localize-all', {
-  shop: 'getoify-test.myshopify.com',
-  token: 'shpua_ef789f03a9d1b35c5a8310547fa77a55',
-  tone: 'elegant and professional',
-  glossary: 'checkout, Shopify'
-}, { responseType: 'text' })
-.then(r => console.log(r.data))
+axios.get('https://getoify.vercel.app/status', {
+  params: { shop: 'getoify-test.myshopify.com' }
+})
+.then(r => console.log(JSON.stringify(r.data, null, 2)))
 .catch(e => console.error(e.message));
